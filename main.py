51226f13a58a0bd57cd54156a581e88b12b6f687
ramsumble <ram.sumble@gmail.com>
2023-07-01 15:07:27 +1000

    added error handling for missing files

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import statistics
 import csv
+import os
 
 file_path_1 = "VA.csv"
+file_path_2 = "MX.csv"
+
+#error handling
+def does_file_exist(file_path):
+    if os.path.exists(file_path):
+        return True
+    else:
+        raise Exception(f"{file_path} does not exist")
+
 
 def calculate_from_csv(file_path):
-    with open(file_path, 'r') as f:
+    with open(file_path, 'r') as f: #'r' to read content of csv
         reader = csv.reader(f)
         reader.__next__() # Skip the headers in first row
         data = []
@@ -15,4 +25,10 @@ def calculate_from_csv(file_path):
         calc_std = statistics.stdev(data)
         return calc_std
 
-print(calculate_from_csv(file_path_1))
+
+try: 
+    print(calculate_from_csv(file_path_1))
+    print(calculate_from_csv(file_path_2))
+except Exception as error:
+     print(str(error))
+
