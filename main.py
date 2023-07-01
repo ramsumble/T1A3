cab7b1abc8e40473fac9c197c3a8905f51808624
ramsumble <ram.sumble@gmail.com>
2023-07-01 21:49:01 +1000

    added calc for second data set, visual data added in form of a linegraph

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import statistics
 import csv
 import os
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+import numpy as np
+import seaborn as sns
+
 
 file_path_1 = "VA.csv"
 file_path_2 = "MX.csv"
@@ -18,7 +23,6 @@ def calculate_from_csv(file_path):
     with open(file_path, "r") as f: #"r" to read content of csv
         reader = csv.reader(f)
         next(reader) # Skip the headers in first row
-        # reader.__next__() 
         data = []
         for row in reader:
                 values = row[2]
@@ -32,19 +36,40 @@ def calculate_from_csv(file_path):
         calc_std = statistics.stdev(data)
         return calc_std
 
-output_value = calculate_from_csv(file_path_1)
+output_value_VA = calculate_from_csv(file_path_1)
+output_value_MX = calculate_from_csv(file_path_2)
 
 #create the csv is none exists, append the calc value to new csv
 def output_csv(file_path):
     with open("output.csv", "a") as file: # "a" to append
-        file.write(f"\n{output_value}")#write() can only write strings - not sure if there is a better way
+        file.write(f"\n{output_value_VA}\n{output_value_MX}")#write() can only write strings - not sure if there is a better way
     return file_path
 
 
+def read_csv(file, delimiter):
+    list_third_column = []
+    with open(file, newline='') as f:
+        reader = csv.reader(f)
+        next(reader)
+        data = list(reader)
+        for item in reversed(data): #some reason need to reverse data to display properly 
+            if item[2] != "null": #remove null values 
+                list_third_column.append(item[2])
+    return list_third_column
+
+
+def plot_data(file):
+    sns.set_theme() # added a theme so the graph is a little easier to digest 
+    data = read_csv(file, ",")
+    plt.plot(data)
+    plt.show()
+
 try: 
     output_csv(output_file)
-    print(calculate_from_csv(file_path_1))
-    print(calculate_from_csv(file_path_2))
+    print("VA score = ",calculate_from_csv(file_path_1))
+    print("MX score = ",calculate_from_csv(file_path_2))
+    plot_data(file_path_1)
+    plot_data(file_path_2)
 
 except Exception as error:
      print(error)
@@ -53,5 +78,7 @@ except Exception as error:
 
 # TODO
 # add calc for mean
-# calc second csv 
 # add date/time 
+# BONUS
+# add a way to mass calculate multiple csv's of the same dataset
+# print the top outliers - need to workout is there is math behind calculating an outlier
