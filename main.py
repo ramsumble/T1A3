28bac391cc821fadfb91d272b7b22c0b2e5829e7
ramsumble <ram.sumble@gmail.com>
2023-07-02 19:11:40 +1000

    added headers to output csv, corrected the output to be in the correct cells and added datetime

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 import numpy as np
 import seaborn as sns
-
+import datetime
 
 file_path_1 = "VA.csv"
 file_path_2 = "MX.csv"
@@ -43,7 +43,13 @@ output_value_MX = calculate_from_csv(file_path_2)
 #create the csv is none exists, append the calc value to new csv
 def output_csv(file_path):
     with open("output.csv", "a") as file: # "a" to append
-        file.write(f"\n{output_value_VA}\n{output_value_MX}")#write() can only write strings - not sure if there is a better way
+        headerlist = ["Date","VA Std", "VA Mean", "MX Std", "MX Mean"]
+        file_is_empty = os.stat(file_path).st_size == 0 #determine if file is empty, only used for the header
+        date = datetime.date.today()
+        if file_is_empty:
+            dw = csv.DictWriter(file, delimiter=",", fieldnames=headerlist)
+            dw.writeheader()
+        file.write(f"{date},{output_value_VA},{output_value_MX}\n")#write() can only write strings - not sure if there is a better way
     return file_path
 
 
@@ -69,16 +75,16 @@ try:
     output_csv(output_file)
     print("VA score = ",calculate_from_csv(file_path_1))
     print("MX score = ",calculate_from_csv(file_path_2))
-    plot_data(file_path_1)
+    # plot_data(file_path_1)
 
 except Exception as error:
      print(error)
 
 
-
 # TODO
-# add calc for mean
 # add date/time 
+# correct the output, be in seperate cells 
+# add help function if applicable
 # BONUS
 # add a way to mass calculate multiple csv's of the same dataset
 # print the top outliers - need to workout is there is math behind calculating an outlier
